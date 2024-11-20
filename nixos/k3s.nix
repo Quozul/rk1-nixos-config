7268329c35fe0ded713eb125f5d74b9e4617b004
Quozul <30729291+Quozul@users.noreply.github.com>
2024-11-20 15:50:03 +0100

    feat: add node exporter

--- nixos/k3s.nix
+++ nixos/k3s.nix
@@ -7,6 +7,8 @@
         6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
         # 2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
         # 2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
+        10250
+        9100 # for prometheus node exporter
     ];
     networking.firewall.allowedUDPPorts = [
         8472 # k3s, flannel: required if using multi-node for inter-node networking
@@ -16,7 +18,7 @@
         enable = true;
         role = "agent";
         token = "Kjtf4pa*BWj(!=(s";
-        serverAddr = "https://192.168.1.141:6443";
+        serverAddr = "https://192.168.3.101:6443";
     };
 
     # For Longhorn support
@@ -25,4 +27,10 @@
       enable = true;
       name = "${config.networking.hostName}-initiatorhost";
     };
+
+    # https://takingnotes.net/kubernetes/longhorn/
+    system.activationScripts.usrlocalbin = ''
+        mkdir -m 0755 -p /usr/local
+        ln -nsf /run/current-system/sw/bin /usr/local/
+    '';
 }
